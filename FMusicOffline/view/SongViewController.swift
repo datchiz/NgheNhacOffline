e84f14e883eaddf371793645d680352383ec113e
datchiz <mai.quoc.dat@vn-macbook-0068s-Mac-mini.local>
2018-01-08 09:18:28 +0700

    add cover flow

---
+++ FMusicOffline/view/SongViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FMusicOffline
+//
+//  Created by mai.quoc.dat on 1/3/18.
+//  Copyright © 2018 com.framgia. All rights reserved.
+//
+
+import UIKit
+
+class SongViewController: UITableViewController {
+    
+    @IBOutlet weak var carousel: iCarousel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        carousel.type = .rotary
+        carousel.backgroundColor = .clear
+        tableView.backgroundView = UIImageView(image: UIImage(named: "background"))
+    }
+}
+
+extension SongViewController : iCarouselDataSource {
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return 10
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        let imageView: UIImageView
+        
+        if view != nil {
+            imageView = view as! UIImageView
+        } else {
+            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+        }
+        
+        imageView.image = UIImage(named: "song")
+        
+        return imageView
+    }
+}
+
